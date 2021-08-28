682c7d8575d1113a8d0ed7ece77ff75bce447ca8
cooper784 <vicox30@gmail.com>
2021-08-27 23:13:42 -0400

    POLIMORFISMO

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,14 @@ import java.util.Calendar;
 public class Main {
     public static void main(String[] args) {
 
-    InforData inforData = new InforData("mxt","america",12345678,10,1293);
-    System.out.println(inforData);
-        Proveedores BussinesTadeito = new Proveedores("Tadeo","La paz",6536782,2,6576, Calendar.getInstance().getTime(),12,"Monitores");
-        System.out.println(BussinesTadeito);
-    Catalogos catalogo1 = new Catalogos("asus",200,343,2600,144,"1080p",10,"monitores");
-        System.out.println(catalogo1);
-        Cliente clien0= new Cliente("ariel","lopv",78322334,Calendar.getInstance().getTime(), 30,2,"monitor",333);
-        System.out.println(clien0);
 
-    Soporte soportetest = new Soporte("seaquemao","ariel",Calendar.getInstance().getTime(),Calendar.getInstance().getTime(),"monitor",333);
-        System.out.println(soportetest);
+        //Polimorfismo
+    Catalogos L4150 = new Catalogos("L4150",1150,4150,2,30,"hd",1,"impresoras");
+        Catalogos impresora = (Catalogos) L4150;
+        System.out.println(impresora);
+
+
+
     }
 
 
